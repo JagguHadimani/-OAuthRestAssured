cfe766ed7273595edbd607131bc1071fccd3e26a
Jagadish Hadimani <jagadishhadimani@hotmail.com>
2026-02-23 15:27:22 +0530

    Add comments for trial Git operations in oAuthTest

--- src/demo/oAuthTest.java
+++ src/demo/oAuthTest.java
@@ -4,8 +4,12 @@ public class oAuthTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Trial pull and push via GIT
+		//Trial pull and push via GIT
+		//Trial pull and push via GIT
 		given().form
 
 	}
 
 }
+
